e5e16888da653ddc89a570f80fc13acc1c6eda4c
ss9501 <1162882015@qq.com>
2018-07-10 16:33:49 +0800

    dele(js): remove useless component

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 export {CompanyLayout} from './js/CompanyLayout';
 export {NormalLayout} from './js/NormalLayout';
 
-export {message} from './js/components/message';
-export {notify} from './js/components/notify';
-export {Pagination} from './js/components/Pagination';
